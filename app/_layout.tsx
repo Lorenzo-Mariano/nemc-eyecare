dc45ff283c15c1cbd92c8d29d639ddec5ed34d8a
Lorenzo-Mariano <hhumminah@gmail.com>
2024-10-02 12:10:52 +0800

    Start adding drawer alongside tab nav

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,22 @@
-import { Stack } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Drawer } from "expo-router/drawer";
 
 export default function RootLayout() {
 	return (
-		<Stack>
-			<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-		</Stack>
+		<GestureHandlerRootView>
+			<Drawer>
+				<Drawer.Screen name="(tabs)" />
+			</Drawer>
+		</GestureHandlerRootView>
 	);
 }
+
+// import { Stack } from "expo-router";
+
+// export default function RootLayout() {
+// 	return (
+// 		<Stack>
+// 			<Stack.Screen name="(drawer)" options={{ headerShown: false }} />
+// 		</Stack>
+// 	);
+// }
