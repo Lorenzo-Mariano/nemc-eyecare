e11024399ec9a67acc4d8c13aa1b52afbfa98c83
Lorenzo-Mariano <hhumminah@gmail.com>
2024-10-02 12:05:39 +0800

    Add custom header component

---
+++ components/Header.tsx
@@ -0,0 +1,31 @@
+import { Text } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { Colors } from "@/constants/Colors";
+import { Sizes } from "@/constants/Sizes";
+
+export default function Header({ title }: { title: string }) {
+	const scheme = useColorScheme();
+
+	return (
+		<SafeAreaView
+			style={{
+				alignItems: "center",
+				padding: Sizes.padding.normal,
+				backgroundColor:
+					scheme === "dark" ? Colors.dark.background : Colors.light.background,
+			}}
+		>
+			<Text
+				style={{
+					color: scheme === "dark" ? Colors.dark.text : Colors.light.text,
+					fontSize: Sizes.text.larger,
+					fontWeight: "bold",
+				}}
+			>
+				{title}
+			</Text>
+		</SafeAreaView>
+	);
+}
