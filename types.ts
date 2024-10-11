c7787fc8585984af23460fb859cc78a996fd787f
Lorenzo-Mariano <hhumminah@gmail.com>
2024-10-11 08:51:14 +0800

    Add RegisterFormData interface

--- types.ts
+++ types.ts
@@ -10,3 +10,15 @@ export interface User {
 	createdAt: string; // Use string if it's an ISO date format
 	updatedAt: string; // Use string if it's an ISO date format
 }
+
+export interface RegisterFormData {
+	firstName: string;
+	lastName: string;
+	email: string;
+	birthday: Date;
+	gender: string;
+	civilStatus: string;
+	phoneNumber: string;
+	password: string;
+	confirmPassword: string;
+}
