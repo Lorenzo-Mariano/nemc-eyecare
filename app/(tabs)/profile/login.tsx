e1ea43d4a788917ec6e280c2c4a98c0059cfd5ab
Lorenzo-Mariano <hhumminah@gmail.com>
2024-10-21 12:01:06 +0800

    Redirect to profile page after successful login

--- app/(tabs)/profile/login.tsx
+++ app/(tabs)/profile/login.tsx
@@ -1,9 +1,19 @@
 import { Colors } from "@/constants/Colors";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import React, { useState } from "react";
-import { View, Text, TextInput, Button, StyleSheet, Alert } from "react-native";
+import {
+	View,
+	Text,
+	TextInput,
+	StyleSheet,
+	Alert,
+	TouchableOpacity,
+} from "react-native";
+import { useRouter } from "expo-router";
+import { Sizes } from "@/constants/Sizes";
 
 export default function Login() {
+	const router = useRouter();
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 	const [isSubmitting, setSubmitting] = useState(false);
@@ -41,56 +51,81 @@ export default function Login() {
 					})
 				);
 
-				Alert.alert("Login Successful", `Welcome back, ${user.firstName}!`);
-				setSubmitting(false);
+				Alert.alert("Login Successful", `Welcome back, ${user.firstName}!`, [
+					{
+						text: "OK",
+						onPress: () => {
+							if (result.message === "Login successful!") {
+								router.replace("/(tabs)/profile");
+							}
+						},
+					},
+				]);
 			} else {
-				setSubmitting(false);
 				Alert.alert("Login Failed", result.message);
 			}
 		} catch (error) {
-			setSubmitting(false);
 			console.error("Login error", error);
 			Alert.alert("Error", "Something went wrong, please try again.");
+		} finally {
+			setSubmitting(false);
 		}
 	};
 
 	return (
-		<View style={styles.container}>
-			<Text style={styles.title}>Login</Text>
+		<View style={styles.page}>
+			<View style={styles.container}>
+				<Text style={styles.title}>Login</Text>
 
-			<TextInput
-				style={styles.input}
-				placeholder="Email"
-				value={email}
-				onChangeText={setEmail}
-				keyboardType="email-address"
-				autoCapitalize="none"
-			/>
+				<TextInput
+					style={styles.input}
+					placeholder="Email"
+					value={email}
+					onChangeText={setEmail}
+					keyboardType="email-address"
+					autoCapitalize="none"
+				/>
 
-			<TextInput
-				style={styles.input}
-				placeholder="Password"
-				value={password}
-				onChangeText={setPassword}
-				secureTextEntry
-			/>
+				<TextInput
+					style={styles.input}
+					placeholder="Password"
+					value={password}
+					onChangeText={setPassword}
+					secureTextEntry
+				/>
 
-			<Button
-				title={isSubmitting ? "Logging in..." : "Login"}
-				onPress={() => handleLogin(email, password)}
-				disabled={isSubmitting}
-			/>
+				<TouchableOpacity
+					onPress={() => handleLogin(email, password)}
+					style={styles.loginBtn}
+				>
+					<Text style={styles.btnText}>
+						{isSubmitting ? "Logging in..." : "Login"}
+					</Text>
+				</TouchableOpacity>
+			</View>
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
-	container: {
+	page: {
 		flex: 1,
 		justifyContent: "center",
+		padding: Sizes.padding.large,
+		backgroundColor: Colors.light.darkerBackground,
+	},
+
+	container: {
+		height: 400,
 		padding: 20,
+		borderRadius: 12,
+
+		borderColor: "#ccc",
+		borderWidth: 1,
+		justifyContent: "center",
 		backgroundColor: Colors.light.background,
 	},
+
 	title: {
 		fontSize: 24,
 		fontWeight: "bold",
@@ -103,5 +138,18 @@ const styles = StyleSheet.create({
 		padding: 10,
 		marginBottom: 15,
 		borderRadius: 5,
+	},
+
+	loginBtn: {
+		padding: Sizes.padding.large,
+		borderColor: Colors.light.theme,
+		borderWidth: 1,
+		borderRadius: 8,
+	},
+
+	btnText: {
+		color: Colors.light.theme,
+		fontWeight: "bold",
+		textAlign: "center",
 	},
 });
